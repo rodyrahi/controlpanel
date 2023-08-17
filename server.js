590dce954b0e67056ea1ef3ed23cdf8be92f0519
rodyrahi <rodyrahi126@gmail.com>
2023-08-17 00:28:20 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 9111;
+const { exec } = require("child_process");
+
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  const command = "pm2 jlist";
+
+  var result ;
+  var std ='' ; 
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      // Handle error
+      result = error;
+    }
+    if (stderr) {
+
+      std = `${stderr}`;
+    }
+    
+    // Store stdout and render the view here, inside the callback
+    std = `${stdout}`;
+
+    
+
+    std = JSON.parse(std);
+
+   
+    res.render("server/server", { result: std,error:`${error}`, stderr:`${stderr}` , stdout:`${stdout}` });
+  });
+});
+
+
+app.post("/cmd", (req, res) => {
+  console.log(req.body);
+  const {command } = req.body
+
+  var result ;
+  var std ='' ; 
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      // Handle error
+      result = error;
+    }
+    if (stderr) {
+
+      std = `${stderr}`;
+    }
+    
+    // Store stdout and render the view here, inside the callback
+    std = `${stdout}`;
+
+
+
+ 
+    console.log(std);
+
+   
+    res.redirect("/");
+  });
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+  
