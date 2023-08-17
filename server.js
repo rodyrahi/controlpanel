d04f4bc516528de455949e9b15f87405faa8f642
rodyrahi <rodyrahi126@gmail.com>
2023-08-17 00:55:57 -0700

    sa

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const { exec } = require("child_process");
 
 
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
