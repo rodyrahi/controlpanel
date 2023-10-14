5e869b641a6cfc47015150333c9f74f13f0bf2a5
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-10-14 17:35:01 +0530

    dasd

--- server.js
+++ server.js
@@ -5,37 +5,23 @@ const port = 9111;
 const { exec } = require("child_process");
 const http = require('http');
 const socketIO = require('socket.io');
-const { stderr } = require("process");
-const Database = require('better-sqlite3');
-const {listDBFiles} = require('./public/database.js');
-// const notifier = require('node-notifier');
-const path = require('path');
 const cron = require('node-cron');
-const { log } = require("console");
 const webPush = require('web-push');
-
 const pm2 = require('pm2');
 
+// Configure Express
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-
-
-
-app.use(express.urlencoded({ extended: true }));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Create an HTTP server and WebSocket
 const server = http.createServer(app);
 const io = socketIO(server);
 
-
-console.log('test');
-const directoryPath = '../database/bundeli';
-
-
-
+// Initialize Web Push with your VAPID keys
+  
 const vapidKeys = {
   publicKey: 'BP__PbJMkGDI6ZgZ1MsR3btGgql-bbBf5wTyZWWOWaQns8ibH8g6410VdYfx9T4_bNOlZVLJz5AA2_JYjh0YzTo',
   privateKey: 'i81qgoWJqCsiWsYODRqYllgDRlqO-SjWfYU8xh2VIqE',
@@ -47,282 +33,96 @@ webPush.setVapidDetails(
   vapidKeys.privateKey
 );
 
-// Initialize Web Push
+// Store subscribers for Web Push notifications
 const subscribers = [];
 
+// Route to subscribe to notifications
 app.use(express.json());
-
 app.post('/subscribe', (req, res) => {
   const subscription = req.body;
   subscribers.push(subscription);
   res.status(201).json({ message: 'Subscribed successfully' });
 });
 
-
-
-// Schedule the check every 5 seconds (adjust as needed)
+// Schedule PM2 process checking every 5 seconds
 cron.schedule('*/5 * * * * *', checkPM2Processes);
 
+// Function to check PM2 processes
 function checkPM2Processes() {
-  // Replace this with your PM2 process checking logic
-  const stoppedApps = ['App1', 'App2']; // Example stopped apps
+  pm2.list((err, processList) => {
+    if (err) {
+      console.error('PM2 list error:', err);
+      return;
+    }
 
-  if (stoppedApps.length > 0) {
-    const message = `The following PM2 app(s) are stopped: ${stoppedApps.join(', ')}`;
-    sendNotifications(message);
-  }
+    const stoppedApps = processList.filter((app) => app.pm2_env.status === 'stopped');
+
+    if (stoppedApps.length > 0) {
+      const appNames = stoppedApps.map((app) => app.name).join(', ');
+      const message = `The following PM2 app(s) are stopped: ${appNames}`;
+      sendNotifications(message);
+    }
+  });
 }
 
+// Function to send Web Push notifications
 function sendNotifications(message) {
   subscribers.forEach((subscription) => {
-    webPush.sendNotification(subscription, JSON.stringify({ title: 'App Stopped', message }));
+    webPush.sendNotification(subscription, JSON.stringify({ title: 'App Stopped', message }))
+      .catch((error) => {
+        console.error('Web Push notification error:', error);
+      });
   });
 }
 
-
-
-
-self.addEventListener('push', (event) => {
-  const options = {
-    body: event.data.text(),
-   // Replace with your icon path
-  };
-
-  event.waitUntil(
-    self.registration.showNotification('App Stopped', options)
-  );
-});
-
-
+// Route to serve the main page
 app.get("/", (req, res) => {
-  const command = "pm2 jlist";
-
-  var result ;
-  var std ='' ; 
-
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      // Handle error
-      result = error;
+  // Replace this with your logic to get PM2 process information
+  pm2.list((err, processList) => {
+    if (err) {
+      console.error('PM2 list error:', err);
+      return;
     }
-    if (stderr) {
-
-      std = `${stderr}`;
-    }
-    
-    // Store stdout and render the view here, inside the callback
-    std = `${stdout}`;
-
-    
-
-    std = JSON.parse(std);
-
-   
-    res.render("server", { result: std,error:`${error}`, stderr:`${stderr}` , stdout:`${stdout}` });
+    res.render("server", { processList });
   });
 });
 
-
+// Route to execute commands
 app.post("/cmd", (req, res) => {
-  console.log(req.body);
-  const {command } = req.body
+  const { command } = req.body;
 
-
-
-  const pm2 = exec(command);
+  const pm2Process = exec(command);
 
   let logContent = '';
-  pm2.stdout.on('data', (data) => {
-      logContent += data.toString();
+  pm2Process.stdout.on('data', (data) => {
+    logContent += data.toString();
   });
 
-  pm2.on('exit', () => {
-      const logLines = logContent.split('\n');
-      const last15Lines = logLines.slice(-20);
+  pm2Process.on('exit', () => {
+    const logLines = logContent.split('\n');
+    const last15Lines = logLines.slice(-20);
 
-      // Format the last 15 lines for display, highlighting errors in red
-      let formattedLog = '<pre>';
-      last15Lines.forEach((line) => {
-          if (line.includes('error') || /error|fatal/i.test(line)) {
-              formattedLog += `<span style="color: red">${line}</span>\n`;
-          } else {
-              formattedLog += line + '\n';
-          }
-      });
-      formattedLog += '</pre>';
-
-      // Replace <br> with newline characters (\n)
-      formattedLog = formattedLog.replace(/<br>/g, '\n');
-
-      res.send(formattedLog);
-  });
-
-
-
-
-
-});
-
-
-
-
-
-
-// Wrap your code in a function
-function runCode(apps) {
-
-}
-
-
-
-
-// app.get("/logs/:apps", (req, res) => {
-
-
-  
-//   const apps = req.params.apps
-  
-//   const dataInterval = setInterval(() => {
-//     io.on('connection', (socket) => {
-//       console.log('Client connected');
-  
-//       const command = `pm2 logs ${apps ? apps : ''} --nostream`;
-  
-//       const process = exec(command);
-//       process.stdout.on('data', (data) => {
-//         var logLine = data.toString().trim();
-//         socket.emit('log', logLine);
-  
-//       });
-  
-//       process.on('close', () => {
-//         console.log('Process closed');
-//         process.kill();
-//       });
-  
-//       socket.on('disconnect', () => {
-//         console.log('Client disconnected');
-//         clearInterval(dataInterval);
-//         process.kill(); // Kill the process when the client disconnects
-  
-//       });
-//     });
-//     // socket.emit('logs', { std: std, stdout: stderr, result: result });
-//   }, 5000);
-//     res.render('logs');
-
-// });
-
-app.get('/db', async (req, res) => {
-  try {
-    const dbFiles = await listDBFiles(directoryPath); // Replace directoryPath with your database directory
-    // const db = new Database(dbFiles[0]) 
-  
-
-
-
-    res.render('database', { db: dbFiles });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
-function querys( element,query ,dbquery) {
-  const dbPath = path.join(directoryPath, dbquery);
-  const db = new Database(dbPath);
-  return db.prepare(query).all()
-}
-
-
-app.post('/query', async(req, res) => {
-  try {
-    const dbFiles = await listDBFiles(directoryPath); // Replace directoryPath with your database directory
-
-    const {query , dbquery} = req.body;
-    var result
-  
-    dbFiles.forEach(element => {
-
-      if (element === dbquery) {
-          console.log(element , dbquery);
-          result = querys(element , query , dbquery)
-
-
-        }
-
+    // Format the last 15 lines for display, highlighting errors in red
+    let formattedLog = '<pre>';
+    last15Lines.forEach((line) => {
+      if (line.includes('error') || /error|fatal/i.test(line)) {
+        formattedLog += `<span style="color: red">${line}</span>\n`;
+      } else {
+        formattedLog += line + '\n';
+      }
     });
+    formattedLog += '</pre>';
 
-    res.json(result)
+    // Replace <br> with newline characters (\n)
+    formattedLog = formattedLog.replace(/<br>/g, '\n');
 
-
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
-
-
-// app.js (continued)
-app.get('/terminal', (req, res) => {
-  const term = exec('bash');
-  term.stdout.on('data', (data) => {
-      res.write(data.toString());
-  });
-  term.on('exit', () => {
-      res.end();
+    res.send(formattedLog);
   });
 });
 
-app.get('/log/:apps', (req, res) => {
-  // Use PM2 to retrieve and display logs
-  const apps = req.params.apps
-
-  const pm2 = exec(`pm2 logs ${apps ? apps : ''} --nostream`);
-
-  let logContent = '';
-  pm2.stdout.on('data', (data) => {
-      logContent += data.toString();
-  });
-
-  pm2.on('exit', () => {
-      const logLines = logContent.split('\n');
-      const last15Lines = logLines.slice(-20);
-
-      // Format the last 15 lines for display, highlighting errors in red
-      let formattedLog = '<pre>';
-      last15Lines.forEach((line) => {
-          if (line.includes('error') || /error|fatal/i.test(line)) {
-              formattedLog += `<span style="color: red">${line}</span>\n`;
-          } else {
-              formattedLog += line + '\n';
-          }
-      });
-      formattedLog += '</pre>';
-
-      // Replace <br> with newline characters (\n)
-      formattedLog = formattedLog.replace(/<br>/g, '\n');
-
-      res.send(formattedLog);
-  });
-});
-
-
-
-
-
-app.get("/test", (req, res) => {
-
-    res.render("test");
-
-});
-
-
-
+// Start the server
 server.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-  
+  console.log(`App is listening on port ${port}`);
+});
+
+
