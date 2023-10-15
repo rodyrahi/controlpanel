ff4df4356f4e58fca1898da6c0b75d0abd6dea7c
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-10-15 20:05:47 +0530

    done bitch

--- server.js
+++ server.js
@@ -1,128 +1,172 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
 const app = express();
-const port = 9111;
-const { exec } = require("child_process");
-const http = require('http');
-const socketIO = require('socket.io');
-const cron = require('node-cron');
-const webPush = require('web-push');
-const pm2 = require('pm2');
-
-// Configure Express
-app.use(express.static('public'));
+const bodyParser = require('body-parser');
+const { NodeSSH } = require('node-ssh');
+const SSH = require('simple-ssh');
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.static('public'));
 
-// Create an HTTP server and WebSocket
-const server = http.createServer(app);
-const io = socketIO(server);
+const ssh = new NodeSSH();
 
-// Initialize Web Push with your VAPID keys
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+
+app.get('/command/:cmd', async (req, res) => {
+
+    const cmd  = req.params.cmd
+
+    console.log(cmd);
+    try {
+   
+        const { stdout, stderr } = await ssh.execCommand(cmd);
+        
+        res.redirect('/home');
+
+    } catch (error) {
+        res.status(500).send(`Error retrieving PM2 apps: ${error.message}`);
+    } 
+});
+
+
+app.post('/connect', async (req, res) => {
+    const { host, username, password } = req.body;
+
+    try {
+        await ssh.connect({
+            host,
+            username,
+            password,
+        });
+
+
+        res.redirect('/home');
+        // res.json(apps)
+
+    } catch (error) {
+        res.send('Failed to connect to the SSH server.');
+    }
+});
+
+app.get('/home', async (req, res) => {
+    try {
+        const [lsResult, pm2Result] = await Promise.all([
+            ssh.execCommand('ls'),
+            ssh.execCommand('pm2 jlist')
+        ]);
+
+        // Process lsResult and pm2Result in parallel
+        const [folder, appList] = await Promise.all([
+            processLsResult(lsResult),
+            processPm2Result(pm2Result)
+        ]);
+
+        res.render('index', { folder, apps: appList });
+    } catch (error) {
+        res.send('Failed to connect to the SSH server or encountered an error.');
+    }
+});
+
+async function processLsResult(lsResult) {
+    const folder = lsResult.stdout.split('\n').filter(Boolean);
+    return folder;
+}
+
+async function processPm2Result(pm2Result) {
+    const appList = JSON.parse(pm2Result.stdout);
+
+    return appList;
+}
+
+
+
+
+app.get('/pm2-apps', async (req, res) => {
+    try {
+        // Execute the 'pm2 jlist' command to get a JSON representation of PM2 apps
+        const { stdout, stderr } = await ssh.execCommand('pm2 jlist');
+        
+        // Parse the JSON output
+        const appList = JSON.parse(stdout);
+        
+        // Extract the app names and statuses
+        const apps = appList.map(app => ({ name: app.name, status: app.pm2_env.status , pwd:app.pm2_env.versioning.repo_path }));
+        
+        res.render('apps', { apps });
+    } catch (error) {
+        res.status(500).send(`Error retrieving PM2 apps: ${error.message}`);
+    }
+});
+
+
+app.get('/folders/:dir', async (req, res) => {
+    let folder = [];
+
+    const dir = req.params.dir ? req.params.dir : '/';
+    try {
   
-const vapidKeys = {
-  publicKey: 'BP__PbJMkGDI6ZgZ1MsR3btGgql-bbBf5wTyZWWOWaQns8ibH8g6410VdYfx9T4_bNOlZVLJz5AA2_JYjh0YzTo',
-  privateKey: 'i81qgoWJqCsiWsYODRqYllgDRlqO-SjWfYU8xh2VIqE',
-};
+        
 
-webPush.setVapidDetails(
-  'mailto:rodyrahi126@gmail.com',
-  vapidKeys.publicKey,
-  vapidKeys.privateKey
-);
 
-// Store subscribers for Web Push notifications
-const subscribers = [];
+        // List the contents of the specified directory within the current directory
+        const { stdout, stderr } = await ssh.execCommand('ls', { cwd:`${dir}` });
+        folder = stdout.split('\n').filter(Boolean);
 
-// Route to subscribe to notifications
-app.use(express.json());
-app.post('/subscribe', (req, res) => {
-  const subscription = req.body;
-  subscribers.push(subscription);
-  res.status(201).json({ message: 'Subscribed successfully' });
+        res.render('folders', { folder });
+   
+
+        // Use '\n' to split lines and filter out empty lines
+    } catch (error) {
+        res.status(500).send(`Error listing directory: ${error.message}`);
+        return;
+    }
+
+    
 });
 
-// Schedule PM2 process checking every 5 seconds
-cron.schedule('*/5 * * * * *', checkPM2Processes);
 
-// Function to check PM2 processes
-function checkPM2Processes() {
-  pm2.list((err, processList) => {
-    if (err) {
-      console.error('PM2 list error:', err);
-      return;
-    }
 
-    const stoppedApps = processList.filter((app) => app.pm2_env.status === 'stopped');
 
-    if (stoppedApps.length > 0) {
-      const appNames = stoppedApps.map((app) => app.name).join(', ');
-      const message = `The following PM2 app(s) are stopped: ${appNames}`;
-      sendNotifications(message);
-    }
-  });
+
+app.post('/execute', async (req, res) => {
+    const { command } = req.body;
+
+    // Check if the command is a 'cd' command
+    // if (command.startsWith('cd ')) {
+    //     const newDirectory = command.substring(3); // Extract the target directory
+    //     try {
+    //         const { stdout, stderr } = await ssh.execCommand( 'ls', {cwd: newDirectory});
+    //         res.send(`<pre>${stdout}</pre>`);
+    //     } catch (error) {
+    //         res.send(`Error changing directory to: ${newDirectory}<br>Error: ${error.message}`);
+    //     }
+    // } else {
+        try {
+            const { stdout, stderr } = await ssh.execCommand( command);
+
+            // Check if the command contains 'error' or 'fatal', and highlight them in red
+            const formattedStdout = highlightErrors(stdout);
+            const formattedStderr = highlightErrors(stderr);
+
+            res.send(`<pre>Status ✨:\n${formattedStdout}\n\nErrors 💀:\n${formattedStderr}</pre>`);
+        } catch (error) {
+            res.send(`Error executing the command: ${error.message}`);
+        }
+    
+});
+
+
+
+
+function highlightErrors(text) {
+
+    return text.replace(/error|fatal/gi, (match) => `<span style="color: red">${match}</span>`);
 }
 
-// Function to send Web Push notifications
-function sendNotifications(message) {
-  subscribers.forEach((subscription) => {
-    webPush.sendNotification(subscription, JSON.stringify({ title: 'App Stopped', message }))
-      .catch((error) => {
-        console.error('Web Push notification error:', error);
-      });
-  });
-}
 
-// Route to serve the main page
-app.get("/", (req, res) => {
-  // Replace this with your logic to get PM2 process information
-  pm2.list((err, processList) => {
-    if (err) {
-      console.error('PM2 list error:', err);
-      return;
-    }
-    res.render("server", { result:processList });
-  });
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
-
-// Route to execute commands
-app.post("/cmd", (req, res) => {
-  const { command } = req.body;
-
-  const pm2Process = exec(command);
-
-  let logContent = '';
-  pm2Process.stdout.on('data', (data) => {
-    logContent += data.toString();
-  });
-
-  pm2Process.on('exit', () => {
-    const logLines = logContent.split('\n');
-    const last15Lines = logLines.slice(-20);
-
-    // Format the last 15 lines for display, highlighting errors in red
-    let formattedLog = '<pre>';
-    last15Lines.forEach((line) => {
-      if (line.includes('error') || /error|fatal/i.test(line)) {
-        formattedLog += `<span style="color: red">${line}</span>\n`;
-      } else {
-        formattedLog += line + '\n';
-      }
-    });
-    formattedLog += '</pre>';
-
-    // Replace <br> with newline characters (\n)
-    formattedLog = formattedLog.replace(/<br>/g, '\n');
-
-    res.send(formattedLog);
-  });
-});
-
-// Start the server
-server.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
-});
-
-
