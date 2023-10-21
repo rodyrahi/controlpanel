1fbe20b1b3080e7e5ead8a9d0236d9382bb084b9
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-10-21 13:01:14 +0530

    ok terminal

---
+++ routes/terminal.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const { Server } = require('socket.io');
+const { ssh,  server } = require('../server.js');
+const io = new Server(server);
+var router = express.Router();
+
+
+
+
+router.get('/', (req, res) => {
+  res.render('partials/terminal')
+});
+
+io.on('connection', (socket) => {
+ 
+
+      socket.emit('data', '\r\n*** SSH CONNECTION ESTABLISHED ***\r\n');
+      return ssh.requestShell()
+    .then((shell) => {
+      shell.on('data', (data) => {
+        socket.emit('data', data.toString('binary'));
+      });
+
+      socket.on('data', (data) => {
+        shell.write(data);
+      });
+
+      shell.on('close', () => {
+        ssh.dispose();
+        socket.emit('data', '\r\n*** SSH CONNECTION CLOSED ***\r\n');
+      });
+    })
+    .catch((err) => {
+      socket.emit('data', '\r\n*** SSH CONNECTION ERROR: ' + err.message + ' ***\r\n');
+    });
+});
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,233 +1,203 @@
-const express = require('express');
+const http = require('http');
+
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const { NodeSSH } = require('node-ssh');
-// const SSH = require('simple-ssh');
-const {userdb , scriptsdb} = require('./db');
-const path = require('path');
+const bodyParser = require("body-parser");
+const { NodeSSH } = require("node-ssh");
+const server = http.createServer(app);
+const { userdb, scriptsdb } = require("./db");
+const path = require("path");
 const ssh = new NodeSSH();
-const fs = require('fs');
-const { auth , requiresAuth } = require('express-openid-connect');
+const fs = require("fs");
+const { auth, requiresAuth } = require("express-openid-connect");
+module.exports = { ssh,server };
 
-
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
+
 
 var isWin = process.platform === "win32";
-
-var baseurl = !isWin ? "https://kadmin.online" : "http://localhost:9111" ;
-
+var baseurl = !isWin ? "https://kadmin.online" : "http://localhost:9111";
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: 'TffWbQSxkKRrepwCtiPWs4AB9WXXGf_nE02j_x9rWcJSiRouU1PSliS8DCPO2hb1',
+  secret: "TffWbQSxkKRrepwCtiPWs4AB9WXXGf_nE02j_x9rWcJSiRouU1PSliS8DCPO2hb1",
   baseURL: baseurl,
-  clientID: 'zj17AAKjTh0ZrdOmD3O7hiXTKc1UwHAy',
-  issuerBaseURL: 'https://dev-t42orpastoaad3st.us.auth0.com'
+  clientID: "zj17AAKjTh0ZrdOmD3O7hiXTKc1UwHAy",
+  issuerBaseURL: "https://dev-t42orpastoaad3st.us.auth0.com",
 };
 app.use(auth(config));
 
 
+const terminalRouter = require("./routes/terminal.js");
+
+app.use("/terminal", terminalRouter);
 
 
 
 
-app.get('/server', requiresAuth(), (req, res) => {
-    const user = req.oidc.user.sub;
-    const email = req.oidc.user.email;
-  
-    // Check if the user exists in the database
-    const result = userdb.prepare('SELECT * FROM user WHERE user = ?').get(user);
-  
-    if (result) {
-      // User exists; check if their expiration date is within the limit
-      const expirationDate = new Date(result.expire);
-      const currentDate = new Date();
+
+
+
+app.get("/server", requiresAuth(), (req, res) => {
+  const user = req.oidc.user.sub;
+  const email = req.oidc.user.email;
+
+  // Check if the user exists in the database
+  const result = userdb.prepare("SELECT * FROM user WHERE user = ?").get(user);
+
+  if (result) {
+    // User exists; check if their expiration date is within the limit
+    const expirationDate = new Date(result.expire);
+    const currentDate = new Date();
     //   currentDate.setDate(currentDate.getDate() + 7); // Adjust this limit as needed
-  
 
-    console.log(expirationDate , currentDate);
+    console.log(expirationDate, currentDate);
 
-      if (expirationDate > currentDate) {
-        // The user's expiration date is within the limit, render the 'login' page
-        res.render('login');
-      } else {
-        res.send('buy it now 😄')
-      }
+    if (expirationDate > currentDate) {
+      // The user's expiration date is within the limit, render the 'login' page
+      res.render("login");
     } else {
-      // User doesn't exist, so create a new user with a 7-day trial
-      const today = new Date();
-      today.setDate(today.getDate() + 7);
-      const expire = today.toISOString().split('T')[0];
-  
-      userdb.prepare('INSERT INTO user (user, email, expire) VALUES (?, ?, ?)').run(user, email, expire);
-  
-      // Render the 'login' page since a new user was created
-      res.render('login');
+      res.send("buy it now 😄");
     }
-  });
+  } else {
+    // User doesn't exist, so create a new user with a 7-day trial
+    const today = new Date();
+    today.setDate(today.getDate() + 7);
+    const expire = today.toISOString().split("T")[0];
+
+    userdb
+      .prepare("INSERT INTO user (user, email, expire) VALUES (?, ?, ?)")
+      .run(user, email, expire);
+
+    // Render the 'login' page since a new user was created
+    res.render("login");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+
+
+
   
 
+app.get("/apps", async (req, res) => {
+  try {
+    const [lsResult, pm2Result] = await Promise.all([
+      ssh.execCommand("ls"),
+      ssh.execCommand("pm2 jlist"),
+    ]);
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+    // Process lsResult and pm2Result in parallel
+    const [folder, appList] = await Promise.all([
+      processLsResult(lsResult),
+      processPm2Result(pm2Result),
+    ]);
 
-app.get('/apps', async(req, res) => {
-    try {
-        const [lsResult, pm2Result] = await Promise.all([
-            ssh.execCommand('ls'),
-            ssh.execCommand('pm2 jlist')
-        ]);
-
-        // Process lsResult and pm2Result in parallel
-        const [folder, appList] = await Promise.all([
-            processLsResult(lsResult),
-            processPm2Result(pm2Result)
-        ]);
-
-        res.render('partials/createapp' ,  { folder, apps: appList });
-
-    } catch (error) {
-        res.send('Failed to connect to the SSH server or encountered an error.');
-    }
-
+    res.render("partials/createapp", { folder, apps: appList });
+  } catch (error) {
+    res.send("Failed to connect to the SSH server or encountered an error.");
+  }
 });
 
 
 
-app.get('/gitrepos', async(req, res) => {
-    let folder = [];
-    try {
+app.get("/gitrepos", async (req, res) => {
+  let folder = [];
+  try {
+    const { stdout, stderr } = await ssh.execCommand("ls", {
+      cwd: `/root/app`,
+    });
+    folder = stdout.split("\n").filter(Boolean);
 
-        const { stdout, stderr } = await ssh.execCommand('ls', { cwd:`/root/app` });
-        folder = stdout.split('\n').filter(Boolean);
-
-        res.render('partials/folders', {folder});
-
-    } catch (error) {
-        res.status(500).send(`Error listing directory: ${error.message}`);
-        return;
-    }
-
+    res.render("partials/folders", { folder });
+  } catch (error) {
+    res.status(500).send(`Error listing directory: ${error.message}`);
+    return;
+  }
 });
 
-app.get('/website', (req, res) => {
-    res.render('partials/createwebsite');
-});
-
-
-// app.get('/command/:cmd', async (req, res) => {
-
-//     const cmd  = req.params.cmd
-
-//     console.log(cmd);
-//     try {
-   
-//         const { stdout, stderr } = await ssh.execCommand(cmd);
-        
-//         res.redirect('/dashboard');
-
-//     } catch (error) {
-//         res.status(500).send(`Error retrieving PM2 apps: ${error.message}`);
-//     } 
-// });
-
-
-app.post('/connect', async (req, res) => {
-
-
-        const { host, username, password } = req.body;
-
-        try {
-            await ssh.connect({
-                host,
-                username,
-                password,
-            });
-    
-    
-            res.redirect('/dashboard');
-            // res.json(apps)
-    
-        } catch (error) {
-            res.send('Failed to connect to the SSH server.');
-        }
-
-
-
-});
-
-app.get('/dashboard', async (req, res) => {
-    
-
-    const result = scriptsdb.prepare('SELECT * FROM scripts').all()
-
-
-    res.render('index.ejs' , {scripts:result});
-});
-
-app.get('/status', async (req, res) => {
-
-  
-    const result = scriptsdb.prepare('SELECT * FROM scripts').all()
-
-
-    res.render('partials/status', {scripts:result} );
-
+app.get("/website", (req, res) => {
+  res.render("partials/createwebsite");
 });
 
 
+app.post("/connect", async (req, res) => {
+  const { host, username, password } = req.body;
+
+  try {
+    await ssh.connect({
+      host,
+      username,
+      password,
+    });
+
+    res.redirect("/dashboard");
+    // res.json(apps)
+  } catch (error) {
+    res.send("Failed to connect to the SSH server.");
+  }
+});
+
+app.get("/dashboard", async (req, res) => {
+  const result = scriptsdb.prepare("SELECT * FROM scripts").all();
+
+  res.render("index.ejs", { scripts: result });
+});
+
+app.get("/status", async (req, res) => {
+  const result = scriptsdb.prepare("SELECT * FROM scripts").all();
+
+  res.render("partials/status", { scripts: result });
+});
 
 async function processLsResult(lsResult) {
-    const folder = lsResult.stdout.split('\n').filter(Boolean);
-    return folder;
+  const folder = lsResult.stdout.split("\n").filter(Boolean);
+  return folder;
 }
 
 async function processPm2Result(pm2Result) {
-    const appList = JSON.parse(pm2Result.stdout);
+  const appList = JSON.parse(pm2Result.stdout);
 
-    return appList;
+  return appList;
 }
 
+app.get("/pm2-apps", async (req, res) => {
+  try {
+    // Execute the 'pm2 jlist' command to get a JSON representation of PM2 apps
+    const { stdout, stderr } = await ssh.execCommand("pm2 jlist");
 
+    // Parse the JSON output
+    const appList = JSON.parse(stdout);
 
+    // Extract the app names and statuses
+    const apps = appList.map((app) => ({
+      name: app.name,
+      status: app.pm2_env.status,
+      pwd: app.pm2_env.versioning.repo_path,
+    }));
 
-app.get('/pm2-apps', async (req, res) => {
-    try {
-        // Execute the 'pm2 jlist' command to get a JSON representation of PM2 apps
-        const { stdout, stderr } = await ssh.execCommand('pm2 jlist');
-        
-        // Parse the JSON output
-        const appList = JSON.parse(stdout);
-        
-        // Extract the app names and statuses
-        const apps = appList.map(app => ({ name: app.name, status: app.pm2_env.status , pwd:app.pm2_env.versioning.repo_path }));
-        
-        res.render('apps', { apps });
-    } catch (error) {
-        res.status(500).send(`Error retrieving PM2 apps: ${error.message}`);
-    }
+    res.render("apps", { apps });
+  } catch (error) {
+    res.status(500).send(`Error retrieving PM2 apps: ${error.message}`);
+  }
 });
-
 
 // app.get('/folders/:dir', async (req, res) => {
 //     let folder = [];
 
 //     const dir = req.params.dir ? req.params.dir : '/';
 //     try {
-  
-        
-
 
 //         // List the contents of the specified directory within the current directory
 //         const { stdout, stderr } = await ssh.execCommand('ls', { cwd:`${dir}` });
 //         folder = stdout.split('\n').filter(Boolean);
 
 //         res.render('folders', { folder });
-   
 
 //         // Use '\n' to split lines and filter out empty lines
 //     } catch (error) {
@@ -235,125 +205,101 @@ app.get('/pm2-apps', async (req, res) => {
 //         return;
 //     }
 
-    
 // });
 
+app.post("/execute", async (req, res) => {
+  const { command } = req.body;
 
+  try {
+    const { stdout, stderr } = await ssh.execCommand(command);
 
+    // Check if the command contains 'error' or 'fatal', and highlight them in red
+    const formattedStdout = highlightErrors(stdout);
+    const formattedStderr = highlightErrors(stderr);
 
-
-app.post('/execute', async (req, res) => {
-    const { command } = req.body;
-
-        try {
-            const { stdout, stderr } = await ssh.execCommand( command);
-
-            // Check if the command contains 'error' or 'fatal', and highlight them in red
-            const formattedStdout = highlightErrors(stdout);
-            const formattedStderr = highlightErrors(stderr);
-
-            res.send(`<pre>Status ✨:\n${formattedStdout}\n\nErrors 💀:\n${formattedStderr}</pre>`);
-        } catch (error) {
-            res.send(`Error executing the command: ${error.message}`);
-        }
-    
+    res.send(
+      `<pre>Status ✨:\n${formattedStdout}\n\nErrors 💀:\n${formattedStderr}</pre>`
+    );
+  } catch (error) {
+    res.send(`Error executing the command: ${error.message}`);
+  }
 });
 
 function highlightErrors(text) {
-
-    return text.replace(/error|fatal/gi, (match) => `<span style="color: red">${match}</span>`);
+  return text.replace(
+    /error|fatal/gi,
+    (match) => `<span style="color: red">${match}</span>`
+  );
 }
 
-app.post('/changedir', async (req, res) => {
-    const { dir  } = req.body;
-   
-    console.log(dir);
-        try {
-            const { stdout, stderr } = await ssh.execCommand(dir);
-            folder = stdout.split('\n').filter(Boolean);
+app.post("/changedir", async (req, res) => {
+  const { dir } = req.body;
 
-            
-            res.render('partials/folders', {folder});
-    
-        } catch (error) {
-            
-            res.send(`Error executing the command: ${error.message}`);
-        }
-    
+  console.log(dir);
+  try {
+    const { stdout, stderr } = await ssh.execCommand(dir);
+    folder = stdout.split("\n").filter(Boolean);
+
+    res.render("partials/folders", { folder });
+  } catch (error) {
+    res.send(`Error executing the command: ${error.message}`);
+  }
 });
 
+app.get("/createwebsite.sh", (req, res) => {
+  const filePath = path.join(__dirname, "createwebsite.sh");
 
-
-
-
-
-
-
-app.get('/createwebsite.sh', (req, res) => {
-    const filePath = path.join(__dirname, 'createwebsite.sh');
-  
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.status(500).send('Error reading the file.');
-      } else {
-        res.setHeader('Content-disposition', 'attachment; createwebsite.sh');
-        res.setHeader('Content-type', 'application/x-sh');
-        res.send(data);
-      }
-    });
-  });
-
-
-app.get('/scripts', (req, res) => {
-    const result = scriptsdb.prepare('SELECT * FROM scripts').all()
-   
-    res.render('partials/scripts' , {result:result})
-});
-app.post('/createscript', (req, res) => {
-    const { scriptname,script , id} = req.body
-
-    const result = scriptsdb.prepare('SELECT * FROM scripts WHERE id= ?').get(id);
-  
-    if (!result) {
-        scriptsdb.prepare(`INSERT INTO scripts (name , script) VALUES (?,?) `).run(scriptname,script) 
-
-    }else{
-        scriptsdb.prepare('UPDATE scripts SET name = ?, script = ? WHERE id = ?').run(scriptname, script, id);
-
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading the file.");
+    } else {
+      res.setHeader("Content-disposition", "attachment; createwebsite.sh");
+      res.setHeader("Content-type", "application/x-sh");
+      res.send(data);
     }
-
-    
-
-    console.log('done');
-    res.redirect('/scripts')
-});
-
-
-
-
-
-app.get('/deletescript/:id', (req, res) => {
-    const id = req.params.id;
-
-    // Assuming you have a database connection and a table named 'scripts'
-    scriptsdb.prepare('DELETE FROM scripts WHERE id = ?').run(id)
-
-           
-    res.redirect('/scripts'); // Redirect to the scripts page after successful deletion
-   
-});
-
-app.get('/sqlite', (req, res) => {
-
-   
-    res.render('partials/sqlite' )
-});
-
-
-
-app.get('/profile', requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.oidc.user));
   });
-app.listen(9111, () => {
-    console.log('Server is running on http://localhost:9111');
+});
+
+app.get("/scripts", (req, res) => {
+  const result = scriptsdb.prepare("SELECT * FROM scripts").all();
+
+  res.render("partials/scripts", { result: result });
+});
+app.post("/createscript", (req, res) => {
+  const { scriptname, script, id } = req.body;
+
+  const result = scriptsdb.prepare("SELECT * FROM scripts WHERE id= ?").get(id);
+
+  if (!result) {
+    scriptsdb
+      .prepare(`INSERT INTO scripts (name , script) VALUES (?,?) `)
+      .run(scriptname, script);
+  } else {
+    scriptsdb
+      .prepare("UPDATE scripts SET name = ?, script = ? WHERE id = ?")
+      .run(scriptname, script, id);
+  }
+
+  console.log("done");
+  res.redirect("/scripts");
+});
+
+app.get("/deletescript/:id", (req, res) => {
+  const id = req.params.id;
+
+  // Assuming you have a database connection and a table named 'scripts'
+  scriptsdb.prepare("DELETE FROM scripts WHERE id = ?").run(id);
+
+  res.redirect("/scripts"); // Redirect to the scripts page after successful deletion
+});
+
+app.get("/sqlite", (req, res) => {
+  res.render("partials/sqlite");
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+server.listen(9111, () => {
+  console.log("Server is running on http://localhost:9111");
 });
