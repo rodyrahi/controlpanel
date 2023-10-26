a35d67af480ab1d781033de29d131f859ec9934d
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-10-27 02:08:24 +0530

    asdasd

--- routes/readfile.js
+++ routes/readfile.js
@@ -9,7 +9,7 @@ var router = express.Router();
 
 router.post('/', async (req, res) => {
   const filePath = req.body.filePath;
-    console.log(filePath);
+    console.log(req.body);
     try {
 
         const result = await ssh.execCommand(`cat ${filePath}`);
