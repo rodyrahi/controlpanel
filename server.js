6bf177f015ac26fdcfe1a8d191d7f29810b4c151
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2024-02-08 14:54:47 +0530

    scripts done

--- server.js
+++ server.js
@@ -447,6 +447,43 @@ app.get("/testcreatewebsite", async (req, res) => {
   res.render("partials/testcreatewebsite")
 });
 
+app.get("/testscripts", async (req, res) => {
+  const scripts = scriptsdb.prepare("SELECT * FROM scripts WHERE user=?").all(req.oidc.user.sub);
+  res.render("partials/testscripts", { scripts})
+});
+
+app.post("/testcreatescript", (req, res) => {
+  const { scriptname, script, id } = req.body;
+
+  const result = scriptsdb.prepare("SELECT * FROM scripts WHERE id= ?").get(id);
+
+  if (!result) {
+    scriptsdb
+      .prepare(`INSERT INTO scripts (name , script ,user) VALUES (?,? ,?) `)
+      .run(scriptname, script, req.oidc.user.sub);
+  } else {
+    scriptsdb
+      .prepare("UPDATE scripts SET name = ?, script = ? WHERE id = ? ")
+      .run(scriptname, script, id);
+  }
+
+  console.log("done");
+  res.redirect("/testscripts");
+});
+
+app.get("/testdeletescript/:id", (req, res) => {
+  const id = req.params.id;
+
+  // Assuming you have a database connection and a table named 'scripts'
+  scriptsdb.prepare("DELETE FROM scripts WHERE id = ?").run(id);
+
+  res.redirect("/testscripts"); // Redirect to the scripts page after successful deletion
+});
+
+
+
+
+
 
 
 
