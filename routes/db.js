3b946205000a450b7647ef851c0325391ac4e4ea
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2024-04-20 11:47:06 +0530

    ll

--- routes/db.js
+++ routes/db.js
@@ -3,7 +3,7 @@ const Database = require('better-sqlite3');
 const userdb = new Database('../database/kadmin/userdb.db');
 const scriptsdb = new Database('../database/kadmin/scriptsdb.db');
 const blogsdb = new Database('../database/kadmin/blogsdb.db');
-
+const systemdb = new Database('../database/kadmin/systemdb.db');
 
 userdb.exec(`
   CREATE TABLE IF NOT EXISTS user (
@@ -49,6 +49,41 @@ blogsdb.exec(`
   )
 `);
 
+systemdb.exec(`
+CREATE TABLE IF NOT EXISTS systemcommands (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  command TEXT,
+  system TEXT
+);
+
+
+
+`);
+
+
+systemdb.exec(`
+INSERT INTO systemcommands (name, command, system)
+SELECT commands.name, commands.command, commands.system FROM (
+  SELECT 'restart-pm2-app' as name, 'pm2 restart $$$' as command, 'linux' as system
+  UNION ALL SELECT 'change-directory', 'cd $$$', 'linux'
+  UNION ALL SELECT 'list-directories', 'ls -la', 'linux'
+  UNION ALL SELECT 'create-directory', 'mkdir $$$', 'linux'
+  UNION ALL SELECT 'remove-directory', 'rm -rf $$$', 'linux'
+  UNION ALL SELECT 'create-file', 'touch $$$', 'linux'
+  UNION ALL SELECT 'delete-pm2-app', 'pm2 delete $$$', 'linux'
+  UNION ALL SELECT 'stop-pm2-app', 'pm2 stop $$$', 'linux'
+  UNION ALL SELECT 'start-pm2-app', 'pm2 start $$$', 'linux'
+  UNION ALL SELECT 'list-pm2-apps', 'pm2 list', 'linux'
+  UNION ALL SELECT 'list-pm2-logs', 'pm2 logs $$$ --stream', 'linux'
+  UNION ALL SELECT 'restart-nginx', 'sudo systemctl restart nginx', 'linux'
+  UNION ALL SELECT 'start-nginx', 'sudo systemctl start nginx', 'linux'
+) AS commands
+LEFT JOIN systemcommands ON commands.name = systemcommands.name
+WHERE systemcommands.name IS NULL;
+`);
+
+
 console.log('Connected to the database');
 
-module.exports = { userdb, scriptsdb , blogsdb };
+module.exports = { userdb, scriptsdb , blogsdb , systemdb };
